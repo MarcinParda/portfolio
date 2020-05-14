083448af5526253e882bed69d3690338c68179f0
MarcinParda <marcin98parda@gmail.com>
2020-05-14 16:46:32 +0200

    Updates

---
+++ precache-manifest.96abc6da5c01b457c84713db8723a623.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be17f3075d7ce2ac7d2051b2ac31d4d4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "be5f2f32a2b08aead632",
+    "url": "/portfolio/static/css/main.6e1c6ba5.chunk.css"
+  },
+  {
+    "revision": "580dc0f2bc5b3d843697",
+    "url": "/portfolio/static/js/2.db9f52f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.db9f52f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be5f2f32a2b08aead632",
+    "url": "/portfolio/static/js/main.496bef46.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "a91eb51a667f7ad9aa9d72ac0e20ac2b",
+    "url": "/portfolio/static/media/album.a91eb51a.png"
+  },
+  {
+    "revision": "ac8f0e01afefb626fae94aac1b109680",
+    "url": "/portfolio/static/media/amarok.ac8f0e01.png"
+  },
+  {
+    "revision": "ff4ced0add18dd55c8fce3ce7ab698b9",
+    "url": "/portfolio/static/media/asteroids.ff4ced0a.png"
+  },
+  {
+    "revision": "0e57232bb568b496842295cbb9ba43b5",
+    "url": "/portfolio/static/media/bem.0e57232b.png"
+  },
+  {
+    "revision": "5710c2fa1e920dcd20419129b2757ae1",
+    "url": "/portfolio/static/media/borrowme.5710c2fa.png"
+  },
+  {
+    "revision": "419d6906d57a0acde23c74bd465b6616",
+    "url": "/portfolio/static/media/css3.419d6906.png"
+  },
+  {
+    "revision": "15a606a05d02b9f2d97bdbe317e8df2e",
+    "url": "/portfolio/static/media/figma.15a606a0.png"
+  },
+  {
+    "revision": "08ec57224f5d2a9929ead8a51b22e6f8",
+    "url": "/portfolio/static/media/git.08ec5722.png"
+  },
+  {
+    "revision": "cbccc53914b68ae25731a3bbd819ff5c",
+    "url": "/portfolio/static/media/github.cbccc539.png"
+  },
+  {
+    "revision": "dee528ba91f82bee47df15ce195a93c5",
+    "url": "/portfolio/static/media/gitlab.dee528ba.png"
+  },
+  {
+    "revision": "88f9e31941f56c08109a36bcf7b3893a",
+    "url": "/portfolio/static/media/html5.88f9e319.png"
+  },
+  {
+    "revision": "0e4fdce8ac22e09688c580e5bc4dcd7d",
+    "url": "/portfolio/static/media/javascript.0e4fdce8.png"
+  },
+  {
+    "revision": "e06c31cbb10a77b07b66d4fad2b0129b",
+    "url": "/portfolio/static/media/mocups.e06c31cb.png"
+  },
+  {
+    "revision": "cdea2de253205de9de5f103f292bbdbc",
+    "url": "/portfolio/static/media/portfolio.cdea2de2.png"
+  },
+  {
+    "revision": "64e5b54e290ab5390c86bb0c5b58f9e6",
+    "url": "/portfolio/static/media/react.64e5b54e.png"
+  },
+  {
+    "revision": "380e22e306f5f0e7ceb6077715651b76",
+    "url": "/portfolio/static/media/sass.380e22e3.png"
+  },
+  {
+    "revision": "97ffebcc0d08059604bc536a28b1795e",
+    "url": "/portfolio/static/media/todolist.97ffebcc.png"
+  },
+  {
+    "revision": "225fbbe09689a4a3219ae1e00fe2280b",
+    "url": "/portfolio/static/media/vscode.225fbbe0.png"
+  }
+]);
